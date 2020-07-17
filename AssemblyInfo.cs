3a105b7f074fbcfec813a17b624e8aca6846c5e4
コルティ・ボイ <admin@kortyboi.com>
2020-07-17 14:58:42 -0400

    Update for v1.2.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -22,4 +22,4 @@ using MelonLoader;
 // Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
 // If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
 // Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
-[assembly: MelonModGame("VRChat", "VRChat")]
+[assembly: MelonModGame]
