5e32788607ff4bc3ed928a66aff1c6276278e552
コルティ・ボイ <admin@kortyboi.com>
2020-08-27 20:39:22 -0400

    Update to v1.3.0 for ML v0.2.6

--- Main.cs
+++ Main.cs
@@ -1,5 +1,4 @@
 using MelonLoader;
-using System.ComponentModel;
 using Console = System.Console;
 
 namespace XSOverlayTitleHider
@@ -9,7 +8,7 @@ namespace XSOverlayTitleHider
         public const string Name = "XSOverlayTitleHider"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "KortyBoi"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.2.1"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.3.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = "https://github.com/KortyBoi/XSOverlayTitleHider"; // Download Link for the Mod.  (Set as null if none)
         public const string Description = "Applies a title change for the MelonLoader Console Window, hiding it from XSOverlay.";
     }
@@ -22,11 +21,11 @@ namespace XSOverlayTitleHider
 
         public override void OnApplicationStart() // Runs after Game Initialization.
         {
-            ModPrefs.RegisterCategory(ModCategory, "Custom Console Title");
+            MelonPrefs.RegisterCategory(ModCategory, "Custom Console Title");
 
-            ModPrefs.RegisterPrefString(ModCategory, CustomTitleText, "Being a Cutie!", "Custom Console Text");
+            MelonPrefs.RegisterString(ModCategory, CustomTitleText, "Being a Cutie!", "Custom Console Text");
 
-            TitleString = ModPrefs.GetString(ModCategory, CustomTitleText);
+            TitleString = MelonPrefs.GetString(ModCategory, CustomTitleText);
 
             Console.Title = string.Format(TitleString);
         }
@@ -35,10 +34,10 @@ namespace XSOverlayTitleHider
         {
             // Highly recommend using UIExpansionKit to easily change your title (For VRChat)
 
-            TitleString = ModPrefs.GetString(ModCategory, CustomTitleText);
+            TitleString = MelonPrefs.GetString(ModCategory, CustomTitleText);
 
             Console.Title = string.Format(TitleString);
-            MelonModLogger.Log("Set console title to: " + TitleString);
+            MelonLogger.Log("Set console title to: " + TitleString);
         }
     }
 }
