8d747efe9b6cc33ff74d8fb1cccef7e192fae86b
Lily <admin@mintlily.lgbt>
2022-08-01 10:33:40 -0400

    Added Game Window Title change (boolean)

--- Main.cs
+++ Main.cs
@@ -1,55 +1,77 @@
 using System;
-using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using MelonLoader;
 using Console = System.Console;
 
-namespace ConsoleTitleHider {
-    public static class BuildInfo {
-        public const string Name = "ConsoleTitleHider";
-        public const string Author = "Lily";
-        public const string Company = null;
-        public const string Version = "2.0.0";
-        public const string DownloadLink = "https://github.com/MintLily/ConsoleTitleHider";
-        public const string Description = "Applies a title change for the MelonLoader Console Window.";
+namespace ConsoleTitleHider;
+public static class BuildInfo {
+    public const string Name = "ConsoleTitleHider";
+    public const string Author = "Lily";
+    public const string Company = "Minty Labs";
+    public const string Version = "2.0.1";
+    public const string DownloadLink = "https://github.com/MintLily/ConsoleTitleHider";
+    public const string Description = "Applies a title change for the MelonLoader Console Window and/or Game Window.";
+}
+public class Main : MelonMod {
+    private MelonPreferences_Category _modCategory;
+    private MelonPreferences_Entry<string> _customTitleText;
+    private MelonPreferences_Entry<bool> _changeGameWindowText;
+    private readonly MelonLogger.Instance _logger = new (BuildInfo.Name, ConsoleColor.DarkYellow);
+    private string _titleString;
+
+    private static (string publisher, string game) GetPublisherAndGameTitle() {
+        var appInfoPath = Path.Combine(MelonUtils.GetGameDataDirectory(), "app.info");
+        var appInfo = File.ReadAllText(appInfoPath);
+        var appInfoLines = appInfo.Split('\n');
+        return (appInfoLines[0], appInfoLines[1]);
     }
 
-    public class Main : MelonMod {
-        private MelonPreferences_Category ModCategory;
-        private MelonPreferences_Entry<string> CustomTitleText;
-        private readonly MelonLogger.Instance Logger = new MelonLogger.Instance(BuildInfo.Name, ConsoleColor.DarkYellow);
-        private string _titleString;
+    private readonly (string publisher, string game) _titleData = GetPublisherAndGameTitle();
 
-        private static (string publisher, string game) GetPublisherAndGameTitle() {
-            string appInfoPath = Path.Combine(MelonUtils.GetGameDataDirectory(), "app.info");
-            string appInfo = File.ReadAllText(appInfoPath);
-            string[] appInfoLines = appInfo.Split('\n');
-            return (appInfoLines[0], appInfoLines[1]);
+    private string GetFormattedTitleText() {
+        var constructedText = MelonPreferences.GetEntry<string>(_modCategory.Identifier, _customTitleText.Identifier).Value;
+        constructedText = constructedText.Replace("{PUBLISHER}", _titleData.publisher);  
+        constructedText = constructedText.Replace("{GAME}", _titleData.game);
+        return constructedText;
+    }
+    
+    [DllImport("user32.dll", EntryPoint = "SetWindowText")]
+    private static extern bool SetWindowText(IntPtr hwnd, String lpString);
+    
+    [DllImport("user32.dll", EntryPoint = "FindWindow")]
+    private static extern IntPtr FindWindow(String className, String windowName);
+
+    private IntPtr _gameWindow;
+
+    public override void OnApplicationStart() {
+        _modCategory = MelonPreferences.CreateCategory("CustomConsoleTitle", "CustomConsoleTitle");
+        _customTitleText = _modCategory.CreateEntry("TitleText", "Being a Cutie!", "Title Text",
+            "Text to show on the Console Window Title");
+        _changeGameWindowText = _modCategory.CreateEntry("changeGameWindowText", false, "Change Game Window Title",
+            "Change the Game Window Title");
+        
+        _titleString = GetFormattedTitleText();
+        Console.Title = string.Format(_titleString);
+        
+        if (_changeGameWindowText.Value) {
+            _gameWindow = FindWindow(null, _titleData.game);
+            SetWindowText(_gameWindow, _titleString);
         }
+        
+        _logger.Msg($"Set console title to: {_titleString}");
+    }
 
-        readonly (string publisher, string game) _titleData = GetPublisherAndGameTitle();
-
-        private string GetFormattedTitleText() {
-            string constructedText = MelonPreferences.GetEntry<string>(ModCategory.Identifier, CustomTitleText.Identifier).Value;
-            constructedText = constructedText.Replace("{PUBLISHER}", _titleData.publisher);  
-            constructedText = constructedText.Replace("{GAME}", _titleData.game);
-            return constructedText;
+    public override void OnPreferencesSaved() {
+        // Highly recommend using UIExpansionKit to easily change your title (For VRChat or ChilloutVR)
+        _titleString = GetFormattedTitleText();
+        Console.Title = string.Format(_titleString);
+        
+        if (_changeGameWindowText.Value) {
+            _gameWindow = FindWindow(null, _titleData.game);
+            SetWindowText(_gameWindow, _titleString);
         }
-
-        public override void OnApplicationStart() {
-            ModCategory = MelonPreferences.CreateCategory("CustomConsoleTitle", "CustomConsoleTitle");
-            CustomTitleText = ModCategory.CreateEntry("TitleText", "Being a Cutie!", "Title Text",
-                "Text to show on the Console Window Title");
-            _titleString = GetFormattedTitleText();
-            Console.Title = string.Format(_titleString);
-            Logger.Msg($"Set console title to: {_titleString}");
-        }
-
-        public override void OnPreferencesSaved() {
-            // Highly recommend using UIExpansionKit to easily change your title (For VRChat)
-            _titleString = GetFormattedTitleText();
-            Console.Title = string.Format(_titleString);
-            Logger.Msg($"Set console title to: {_titleString}");
-        }
+        
+        _logger.Msg($"Set console title to: {_titleString}");
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Universal MelonLoader Mod, that is used to hide the default title console is given with MelonLoader. This is to hide XSOverlay's Discord Rich Presence game title; or any game or anything that looks/uses the Console Window Title for text, replacing it to Anything of your choosing.<br>
 This mod will work for MelonLoader Modded games using both IL2CPP and MonoBleedingEdge (Mono)
 
-Changes title from `MelonLoader v0.5.3 Open-Beta - {Game}` to Anything of your choosing.
+Changes console title from `MelonLoader v0.5.4 Open-Beta - {Game}` to anything of your choosing.<br>
+Changes the game window title from `{Game}` to anything of your choosing, the same as console title setting.
 
 ### MelonLoader
 Need to install MelonLoader?<br>
@@ -10,19 +11,20 @@ Click [this link](https://melonwiki.xyz/) to get started!
 
 ### How-To
 Manually change it in your `MelonPreferences.cfg` file<br>
-For VRChat, you can use the [UIExpansionKit](https://github.com/knah/VRCMods) Mod to easily apply settings while you're in-game.
+~~For VRChat, you can use the [UIExpansionKit](https://github.com/knah/VRCMods) Mod to easily apply settings while you're in-game.~~
 
 ### MelonPreferences (Default Values)
 ```ini
-[XSOverlayTitleHider]
+[CustomConsoleTitle]
 TitleText = "Being a Cutie!"
+changeGameWindowText = false
 ```
 
 ### Preview
 ![Before After Preview](https://i.mintlily.lgbt/Preview.png)
 
 ### Tested Games
-- VRChat
+- VRChat **(EAC Prevention)**
 - ~~Beat Saber~~
 - BONEWORKS
 - SynthRiders
@@ -30,6 +32,7 @@ TitleText = "Being a Cutie!"
 - Audica
 - H3VR
 - Pistol Whip
+- ChilloutVR
 
 ### Unknown / Untested
 - Phasmophobia
